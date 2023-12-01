0dfeaacadc39b84ba957f7aac46386cb7ce13544
Ahmed Wael Wanas <absolutezero@pop-os.zte.com.cn>
2023-12-01 21:17:20 +0200

    fixed some stuff

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def create_fuzzy_system():
 
     if choice == "2":
         return
-
-    print("""
+    while True:
+        print("""
 Main Menu:
 ==========
 1- Add variables.
@@ -15,39 +15,47 @@ Main Menu:
 3- Add rules.
 4- Run the simulation on crisp values.\n""")
 
-    choice = input()
+        choice = input()
 
-    if choice == "1":
-        print("""
+        if choice == "1":
+            print("""
 Enter the variable’s name, type (IN/OUT), and range ([lower, upper]):
 (Press x to finish)\n""")
-        variables = []
-        while variables[-1] != "x":
-            variables.append(input())
+            variables = []
+            inp = input()
+            while inp != "x":
+                variables.append(inp)
+                inp = input()
 
-    elif choice == "2":
-        print("Enter the variable’s name:\n")
-        variable_name = input()
+        elif choice == "2":
+            print("Enter the variable’s name:\n")
+            variable_name = input()
 
-        print("Enter the fuzzy set name, type (TRI/TRAP), and values: (Press x to finish)\n")
-        fuzzy_sets = []
-        while fuzzy_sets[-1] != "x":
-            fuzzy_sets.append(input())
+            print("Enter the fuzzy set name, type (TRI/TRAP), and values: (Press x to finish)\n")
+            fuzzy_sets = []
+            inp = input()
+            while inp != "x":
+                fuzzy_sets.append(inp)
+                inp = input()
 
-    elif choice == "3":
-        print("Enter the rules in this format: (Press x to finish)\n")
-        print("IN_variable set operator IN_variable set => OUT_variable set\n")
-        rules = []
-        while rules[-1] != "x":
-            rules.append(input())
+        elif choice == "3":
+            print("Enter the rules in this format: (Press x to finish)\n")
+            print("IN_variable set operator IN_variable set => OUT_variable set\n")
+            rules = []
+            inp = input()
+            while inp != "x":
+                rules.append(inp)
+                inp = input()
 
-    elif choice == "4":
-        print("Enter the crisp value:\n")
-        crisp_values = []
-        while crisp_values[-1] != "x":
-            crisp_values.append(input())
+        elif choice == "4":
+            print("Enter the crisp value:\n")
+            crisp_values = []
+            inp = input()
+            while inp != "x":
+                crisp_values.append(inp)
+                inp = input()
 
-    else:
-        return
+        else:
+            return
 
 create_fuzzy_system()
